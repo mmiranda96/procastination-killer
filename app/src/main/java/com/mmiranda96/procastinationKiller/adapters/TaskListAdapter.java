48a881334354ab1f404543efa1d71d13c2fde1a0
Miguel Miranda <mikemp96@gmail.com>
2019-02-24 13:36:35 -0600

    Create Add people activity

--- app/src/main/java/com/mmiranda96/procastinationKiller/adapters/TaskListAdapter.java
+++ app/src/main/java/com/mmiranda96/procastinationKiller/adapters/TaskListAdapter.java
@@ -1,13 +1,16 @@
 package com.mmiranda96.procastinationKiller.adapters;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mmiranda96.procastinationKiller.InvitePeopleActivity;
 import com.mmiranda96.procastinationKiller.R;
 import com.mmiranda96.procastinationKiller.models.Task;
 
@@ -30,17 +33,28 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
+    public View getView(final int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.task_list_view, parent,false);
+
         TextView title = rowView.findViewById(R.id.textViewTaskListAdapterTitle);
         TextView description = rowView.findViewById(R.id.textViewTaskListAdapterDescription);
+        Button addPeople = rowView.findViewById(R.id.buttonTaskListAdapterAddPeople);
 
-        Task task = this.tasks.get(position);
+        final Task task = this.tasks.get(position);
 
         title.setText(task.getTitle());
         description.setText(task.getDescription());
+        addPeople.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context.getApplicationContext(), InvitePeopleActivity.class);
+                intent.putExtra("task", task);
+                context.startActivity(intent);
+            }
+        });
 
         return rowView;
     }
+
 }
