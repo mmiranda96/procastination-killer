9db7eb9ec8ff7f612dedabd82f33d6d66e6483be
Miguel Miranda <mikemp96@gmail.com>
2019-03-31 18:43:42 -0600

    Add signup activity

---
+++ app/src/main/java/com/mmiranda96/procastinationKiller/activities/SignupActivity.java
@@ -0,0 +1,76 @@
+package com.mmiranda96.procastinationKiller.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mmiranda96.procastinationKiller.R;
+import com.mmiranda96.procastinationKiller.models.User;
+import com.mmiranda96.procastinationKiller.sources.user.SignupAsyncTask;
+import com.mmiranda96.procastinationKiller.sources.user.UserSource;
+import com.mmiranda96.procastinationKiller.sources.user.UserSourceFactory;
+import com.mmiranda96.procastinationKiller.util.IntentExtras;
+import com.mmiranda96.procastinationKiller.util.Server;
+
+public class SignupActivity extends AppCompatActivity implements SignupAsyncTask.Listener {
+
+    private EditText email, password, confirmPassword;
+    private UserSource userSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        this.email = findViewById(R.id.editTextSignupActivityEmail);
+        this.password = findViewById(R.id.editTextSignupActivityPassword);
+        this.confirmPassword = findViewById(R.id.editTextSignupActivityConfirmPassword);
+        this.userSource = UserSourceFactory.newSource(UserSourceFactory.REMOTE, Server.URL);
+    }
+
+    public void signup(View view) {
+        if (this.email.getText().toString().compareTo("") == 0) {
+            Toast.makeText(getApplicationContext(), "An email is required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (this.password.getText().toString().compareTo("") == 0) {
+            Toast.makeText(getApplicationContext(), "A password is required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (this.password.getText().toString().compareTo(this.confirmPassword.getText().toString()) != 0) {
+            Toast.makeText(getApplicationContext(), "Passwords must match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String email = this.email.getText().toString();
+        String password = this.password.getText().toString();
+        User user = new User(email, password);
+
+        SignupAsyncTask asyncTask = this.userSource.newSignupAsyncTask(this);
+        asyncTask.execute(user);
+
+    }
+
+    @Override
+    public void signupAsyncTaskDone(Integer result) {
+        switch (result) {
+            case SignupAsyncTask.SUCCESS:
+                String email = this.email.getText().toString();
+                String password = this.password.getText().toString();
+
+                User user = new User(email, password);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra(IntentExtras.USER, user);
+                startActivity(intent);
+                return;
+            case SignupAsyncTask.FAILURE:
+                Toast.makeText(getApplicationContext(), "Email is already in use", Toast.LENGTH_SHORT).show();
+                return;
+            default:
+                Toast.makeText(getApplicationContext(), "An error ocurred", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
