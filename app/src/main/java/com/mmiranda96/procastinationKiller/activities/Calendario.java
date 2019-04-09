92c9e00a0d85371aece69f5fdccac6aa6fd8c22c
alexdelmed <alexdm14@live.com.mx>
2019-04-09 13:39:21 -0500

    calendario

---
+++ app/src/main/java/com/mmiranda96/procastinationKiller/activities/Calendario.java
@@ -0,0 +1,29 @@
+package com.mmiranda96.procastinationKiller.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class Calendario extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + i2 + "/" + i;
+                myDate.setText(date);
+            }
+        });
+    }
+}
